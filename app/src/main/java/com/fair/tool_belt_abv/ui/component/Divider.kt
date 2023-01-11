d823adff67dd772b98b2c6fc4f2fe5100b11d9d5
Revok <cerve.v55@gmail.com>
2023-01-11 09:30:57 -0500

    theming updated with Components and Colors.

---
+++ app/src/main/java/com/fair/tool_belt_abv/ui/component/Divider.kt
@@ -0,0 +1,20 @@
+package com.fair.tool_belt_abv.ui.component
+
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.cerve.co.material3extension.designsystem.ExtendedTheme.colors
+import com.cerve.co.material3extension.designsystem.ExtendedTheme.sizes
+
+@Composable
+fun ThemedDivider(
+    modifier: Modifier = Modifier
+) {
+
+    Divider(
+        modifier = modifier,
+        thickness = sizes.xSmall,
+        color = colors.onSurface
+    )
+
+}
