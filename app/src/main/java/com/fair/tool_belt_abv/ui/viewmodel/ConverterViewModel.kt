cd273c37c748fa3824885d9009e601b4c80bb430
Revok <cerve.v55@gmail.com>
2023-10-18 16:13:08 -0400

    converter moved from view-model to a use-case to allow for better cross platform implementation.

--- app/src/main/java/com/fair/tool_belt_abv/ui/viewmodel/ConverterViewModel.kt
+++ app/src/main/java/com/fair/tool_belt_abv/ui/viewmodel/ConverterViewModel.kt
@@ -1,9 +1,9 @@
 package com.fair.tool_belt_abv.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.fair.tool_belt_abv.domain.ConvertResultUseCase
 import com.fair.tool_belt_abv.model.AbvUnit
 import com.fair.tool_belt_abv.model.ConverterResult
-import com.fair.tool_belt_abv.util.Converter
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -11,27 +11,20 @@ import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class ConverterViewModel @Inject constructor() : ViewModel() {
+class ConverterViewModel @Inject constructor(
+    private val convertResultUseCase: ConvertResultUseCase
+) : ViewModel() {
 
     private val _result = MutableStateFlow(ConverterResult())
     val result = _result.asStateFlow()
 
     fun updateValue(unit: AbvUnit, value: String) {
         _result.update { result ->
-            when (unit) {
-                AbvUnit.SG -> {
-                    val (plato, brix) = Converter.convert(focused = unit.name, value = value)
-                    result.copy(sg = value, plato = plato, brix = brix)
-                }
-                AbvUnit.P -> {
-                    val (brix, gravity) = Converter.convert(focused = unit.name, value = value)
-                    result.copy(plato = value, brix = brix, sg = gravity)
-                }
-                AbvUnit.B -> {
-                    val (gravity, plato) = Converter.convert(focused = unit.name, value = value)
-                    result.copy(brix = value, sg = gravity, plato = plato)
-                }
-            }
+            convertResultUseCase.invoke(
+                unit = unit,
+                value = value,
+                previousResult = result
+            )
         }
     }
 }
